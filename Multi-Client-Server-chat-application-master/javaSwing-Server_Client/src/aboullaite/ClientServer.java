27849ecd497e029ac8f8fbbf267a58a6577dc62f
Raul Montoya <raulomontoyaperez@gmail.com>
2018-10-21 05:58:15 +0200

    Add files via upload

---
+++ Multi-Client-Server-chat-application-master/javaSwing-Server_Client/src/aboullaite/ClientServer.java
@@ -0,0 +1,15 @@
+package aboullaite;
+
+//Class to precise who is connected : Client or Server
+public class ClientServer {
+
+
+	public static void main(String [] args){
+
+        	String[] arguments = new String[] {};
+			new MultiThreadChatServerSync().main(arguments);
+
+
+	}
+
+}
